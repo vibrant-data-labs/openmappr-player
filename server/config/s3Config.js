d9d3b3642bacf29155f6b5e27520977bfcb2f7b4
mike.tulubaev <mike.tulubaev@sharp-dev.net>
2020-11-07 01:19:17 +0500

    fix sample files

--- server/config/s3Config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    accessKeyId: '',
-    secretAccessKey: '',
-    bucketDefaultPrefix: 'mappr-player-version-'
-    bucketDefaultPrefix: 'mappr-player-version-'
-};
--- server/config/s3Config.sample.js
+++ server/config/s3Config.sample.js
@@ -1,3 +1,5 @@
+// Copy the content of this file to the `s3Config.js` of the same folder
+// and change the values according to AWS credentials
 module.exports = {
   accessKeyId: '',
   secretAccessKey: '',
