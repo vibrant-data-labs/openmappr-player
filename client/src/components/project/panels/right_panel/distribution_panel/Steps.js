fc4cf7e42887b3b6cc4dada68030f408f08c87af
mike.tulubaev <mneogoogacc@gmail.com>
2023-02-06 14:17:07 +0500

    DEV-59 clean up redundant files

--- client/src/components/project/panels/right_panel/distribution_panel/Steps.js
+++
@@ -1,5 +0,0 @@
-angular.module('common').constant('constSteps', {
-    main: 'By knowing how this section works, you will be able to browse through data at your convenience, ~as if you were searching through a variety of shoes on zappos to find the perfect pair.~',
-    keyword: 'These are keywords that come up in the data set',
-    category: 'This is better at filtering data by different categories and shows the count of data points in these categories.'
-});
--- server/config/s3Config.sample.js
+++
@@ -1,6 +0,0 @@
-// Copy the content of this file to the `s3Config.js` of the same folder
-// and change the values according to AWS credentials
-module.exports = {
-  accessKeyId: '',
-  secretAccessKey: ''
-};
